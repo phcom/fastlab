388feaaf5d9613a360539649a11243c25c8736bc
Pedro Vieira <phbattistellavieira@gmail.com>
2024-08-19 21:54:06 -0300

    Update pylint threshold and uncomment uvicorn run command
    - Update pylint threshold from 7 to 5 in the GitHub workflow file.
    - Uncomment the uvicorn run command for local execution.

--- app/__main__.py
+++ app/__main__.py
@@ -5,11 +5,10 @@ import uvicorn
 def main():
     """ Main function for running the application.
     """
-    asdasdf
     # DOCKER
-    uvicorn.run("app:create_app", host="0.0.0.0", port=8000)
+    # uvicorn.run("app:create_app", host="0.0.0.0", port=8000)
     # LOCAL
-    #uvicorn.run("app:create_app", factory=True)
+    uvicorn.run("app:create_app", factory=True)
 
 
 if __name__ == "__main__":
